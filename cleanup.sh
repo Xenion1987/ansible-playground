e446f8a04d26fc3378f7867164338ec122334d19
Xenion1987 <39803750+Xenion1987@users.noreply.github>
2023-01-21 23:00:45 +0100

    some more additions

--- cleanup.sh
+++ cleanup.sh
@@ -9,14 +9,14 @@ if [[ -n ${DOCKER_IMAGE_OUTPUT} ]]; then
     docker image rm $(docker image ls -q "ansible_client_*")
 fi
 
-sudo find ./ansible-files -not \( -name "*.example" -or -name "log" -or -name "ansible-files" \)
+#sudo find ./ansible-files -not \( -name "*.example" -or -name "log" -or -name "ansible-files" \)
 sudo find ./ssh-keys -not \( -name "ssh-keys" -or -name "server" -or -name "clients" -or -name ".keep" \)
 echo "This script will delete all files listed above."
 echo "Do you want to continue? [y/N] "
 read -r -n1 yn
 case ${yn} in
 y | Y | j | J)
-    sudo find ./ansible-files -not \( -name "*.example" -or -name "log" -or -name "ansible-files" \) -delete
+    #sudo find ./ansible-files -not \( -name "*.example" -or -name "log" -or -name "ansible-files" \) -delete
     sudo find ./ssh-keys -not \( -name "ssh-keys" -or -name "server" -or -name "clients" -or -name ".keep" \) -delete
     touch ./ssh-keys/server/authorized_keys
     sudo chown 0:0 ./ssh-keys/server/authorized_keys
