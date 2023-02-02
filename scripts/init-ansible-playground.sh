08c2bd4b47393eb56a7c277ab7430674ab247d79
Xenion1987 <39803750+Xenion1987@users.noreply.github>
2023-02-02 20:48:54 +0000

    init OpenSUSE-Leap-15 client

--- scripts/init-ansible-playground.sh
+++ scripts/init-ansible-playground.sh
@@ -1,41 +1,37 @@
 #! /usr/bin/env bash
 
 if [[ ! $(docker network ls -qf name=ansible-playground) =~ [0-9a-z]{12} ]]; then
-    docker network create ansible-playground
+  docker network create ansible-playground
 fi
 
 if docker compose build; then
-    echo "Generate initial SSH keypair"
-    docker run -it --rm \
-        -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
-        -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
-        ansible_client_ubuntu \
-            ssh-keygen -t rsa -b 4096 -f /root/.ssh/server/id_rsa -N '' -C "ansible-playground_$(date +%Y%m%d_%H%M%S)"
-    
-    echo "Add 'authorized_keys' with root's public key for all clients"
-    docker run -it --rm \
-        -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
-        -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
-        ansible_client_ubuntu \
-            cp /root/.ssh/server/id_rsa.pub /root/.ssh/clients/authorized_keys
-    
-    echo "Fix permissions for ansible-control node"
-    docker run -it --rm \
-        -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
-        -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
-        ansible_client_ubuntu \
-            chown -R 1000:1000 /root/.ssh/
+  echo "Generate initial SSH keypair"
+  docker run -it --rm \
+    -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
+    -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
+    ansible_client_ubuntu \
+    ssh-keygen -t rsa -b 4096 -f /root/.ssh/server/id_rsa -N '' -C "ansible-playground_$(date +%Y%m%d_%H%M%S)"
 
-    echo "Creating docker network"    
-    docker network create ansible-playground
+  echo "Add 'authorized_keys' with root's public key for all clients"
+  docker run -it --rm \
+    -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
+    -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
+    ansible_client_ubuntu \
+    cp /root/.ssh/server/id_rsa.pub /root/.ssh/clients/authorized_keys
 
-    echo "Copy 'authorized_keys' into client's root ssh dir"
-    if docker compose up -d; then
-        for s in $(docker compose ps --services --status running); do
-            echo "$s"
-            docker compose exec ${s} mkdir -p /root/.ssh
-            docker compose exec ${s} cp /root/authorized_keys /root/.ssh/authorized_keys
-        done
-    fi
+  echo "Fix permissions for ansible-control node"
+  docker run -it --rm \
+    -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
+    -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
+    ansible_client_ubuntu \
+    chown -R 1000:1000 /root/.ssh/
+
+  echo "Copy 'authorized_keys' into client's root ssh dir"
+  if docker compose up -d; then
+    for s in $(docker compose ps --services --status running); do
+      echo "$s"
+      docker compose exec ${s} mkdir -p /root/.ssh
+      docker compose exec ${s} cp /root/authorized_keys /root/.ssh/authorized_keys
+    done
+  fi
 fi
-
