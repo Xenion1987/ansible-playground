348b470357a2082a62621f03a657ad4593ca4bad
Marvin Moldenhauer <39803750+Xenion1987@users.noreply.github.com>
2023-03-02 20:58:48 +0100

    Move init files into .devcontainer (#22)
    Co-authored-by: Xenion1987 <39803750+Xenion1987@users.noreply.github>

---
+++ .devcontainer/scripts/cleanup.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+#########################################################
+# Call this script from the repository's root dir.
+#########################################################
+
+# shellcheck disable=SC2046
+
+docker compose -f .devcontainer/docker-compose.yml down
+
+o=$(docker container ls -qa -f name="ansible-playground-control")
+if [[ -n $o ]]; then
+  docker container rm -f ${o}
+fi
+o=$(docker image ls -q "ansible-client-*")
+if [[ -n $o ]]; then
+  docker image rm $(docker image ls -q "ansible-client-*")
+fi
+o=$(docker image ls -q "vcs-ansible-playground*")
+if [[ -n $o ]]; then
+  docker image rm $(docker image ls -q "vcs-ansible-playground*")
+fi
+docker builder prune -f
+
+o=$(find ./.devcontainer/ssh-keys -not \( -name "ssh-keys" -or -name "server" -or -name "clients" -or -name ".keep" \))
+if [[ -n $o ]]; then
+  echo "${o}"
+  echo "This script will delete all files listed above."
+  echo "Do you want to continue? [y/N] "
+  read -r -n1 yn
+  case ${yn} in
+  y | Y | j | J)
+    find ./.devcontainer/ssh-keys -not \( -name "ssh-keys" -or -name "server" -or -name "clients" -or -name ".keep" \) -delete
+    echo
+    echo "done"
+    ;;
+  *)
+    echo "Wrong input - Cancelled by user"
+    echo
+    ;;
+  esac
+fi
--- .devcontainer/scripts/init-ansible-playground.sh
+++ .devcontainer/scripts/init-ansible-playground.sh
@@ -8,48 +8,48 @@ while read -r image; do
   if ! docker image ls "${image}" | grep -q "${image}"; then
     build_images+=(${image})
   fi
-done < <(awk '/image:/ {print $NF}' docker-compose.yml)
+done < <(awk '/image:/ {print $NF}' .devcontainer/docker-compose.yml)
 if [[ ${#build_images[@]} -gt 0 ]]; then
   echo "-------------- BUILDING DOCKER IMAGES --------------"
-  docker compose build "${build_images[@]}"
+  docker compose -f .devcontainer/docker-compose.yml build "${build_images[@]}"
 fi
 echo "---------------- DOCKER IMAGES BUILT ---------------"
 
 if [[ ! -f ${PWD}/ssh-keys/clients/authorized_keys ]]; then
-  touch ${PWD}/ssh-keys/clients/authorized_keys
+  touch ${PWD}/.devcontainer/ssh-keys/clients/authorized_keys
 fi
 
-docker compose up -d
+docker compose -f .devcontainer/docker-compose.yml up -d
 
-if ! find ssh-keys -type f -name id_rsa | grep -q .; then
+if ! find .devcontainer/ssh-keys -type f -name id_rsa | grep -q .; then
   echo "---------------- DEPLOY SSH KEYFILES ---------------"
   echo "Generate initial SSH keypair"
   docker run --rm --name tmp_ansible_init -it \
-    -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
-    -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
+    -v ${PWD}/.devcontainer/ssh-keys/clients/:/root/.ssh/clients \
+    -v ${PWD}/.devcontainer/ssh-keys/server/:/root/.ssh/server \
     ansible-client-debian \
     bash -c 'ssh-keygen -q -t rsa -b 4096 -f /root/.ssh/server/id_rsa -N "" -C "ansible-playground_$(date +%Y%m%d_%H%M%S)"<<<y >/dev/null'
 
   echo "Add 'authorized_keys' with root's public key for all clients"
   docker run --rm --name tmp_ansible_init -it \
-    -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
-    -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
+    -v ${PWD}/.devcontainer/ssh-keys/clients/:/root/.ssh/clients \
+    -v ${PWD}/.devcontainer/ssh-keys/server/:/root/.ssh/server \
     ansible-client-debian \
     bash -c 'cp /root/.ssh/server/id_rsa.pub /root/.ssh/clients/authorized_keys'
 
   echo "Fix permissions for ansible-control node"
   docker run --rm --name tmp_ansible_init -it \
-    -v ${PWD}/ssh-keys/clients/:/root/.ssh/clients \
-    -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
+    -v ${PWD}/.devcontainer/ssh-keys/clients/:/root/.ssh/clients \
+    -v ${PWD}/.devcontainer/ssh-keys/server/:/root/.ssh/server \
     ansible-client-debian \
     bash -c 'chown -R 1000:1000 /root/.ssh/'
 fi
 
 echo "Copy 'authorized_keys' into client's root ssh dir"
-for s in $(docker compose ps --services --status running); do
+for s in $(docker compose -f .devcontainer/docker-compose.yml ps --services --status running); do
   echo "${s}"
-  docker compose exec "${s}" mkdir -p /root/.ssh
-  docker compose exec "${s}" chmod 700 /root/.ssh
-  docker compose exec "${s}" cp /root/authorized_keys /root/.ssh/authorized_keys
+  docker compose -f .devcontainer/docker-compose.yml exec "${s}" mkdir -p /root/.ssh
+  docker compose -f .devcontainer/docker-compose.yml exec "${s}" chmod 700 /root/.ssh
+  docker compose -f .devcontainer/docker-compose.yml exec "${s}" cp -v /root/authorized_keys /root/.ssh/authorized_keys
 done
 echo "--------------- SSH KEYFILES DEPLOYED --------------"
