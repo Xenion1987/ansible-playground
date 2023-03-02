46675f6da44136f156c7bfbcdc99e5db8e4b9dbe
Marvin Moldenhauer <39803750+Xenion1987@users.noreply.github.com>
2023-03-02 16:28:28 +0100

    fix/deploy ssh keys twice

--- scripts/init-ansible-playground.sh
+++ scripts/init-ansible-playground.sh
@@ -43,15 +43,8 @@ if ! find ssh-keys -type f -name id_rsa | grep -q .; then
     -v ${PWD}/ssh-keys/server/:/root/.ssh/server \
     ansible-client-debian \
     bash -c 'chown -R 1000:1000 /root/.ssh/'
-
-  echo "Copy 'authorized_keys' into client's root ssh dir"
-  for s in $(docker compose ps --services --status running); do
-    echo "${s}"
-    docker compose exec "${s}" mkdir -p /root/.ssh
-    docker compose exec "${s}" chmod 700 /root/.ssh
-    docker compose exec "${s}" cp /root/authorized_keys /root/.ssh/authorized_keys
-  done
 fi
+
 echo "Copy 'authorized_keys' into client's root ssh dir"
 for s in $(docker compose ps --services --status running); do
   echo "${s}"
