4c9dff8cb94001ffbd00eecf9c4a4f43893542da
Marvin Moldenhauer <marvin.moldenhauer@dogado.de>
2023-10-16 10:27:06 +0200

    commit pre-commit fixes

--- .devcontainer/scripts/cleanup.sh
+++ .devcontainer/scripts/cleanup.sh
@@ -10,7 +10,7 @@ docker compose -f .devcontainer/docker-compose.yml down
 
 o=$(docker container ls -qa -f name="ansible-playground-control")
 if [[ -n $o ]]; then
-  docker container rm -f ${o}
+  docker container rm -f "${o}"
 fi
 o=$(docker image ls -q "ansible-client-*")
 if [[ -n $o ]]; then
