adccbaa1de7e5ff7daf1e498935ab3a8f0785742
Marvin Moldenhauer <marvin.moldenhauer@dogado.de>
2023-10-16 10:27:06 +0200

    commit pre-commit fixes

--- .devcontainer/scripts/post-create-command.sh
+++ .devcontainer/scripts/post-create-command.sh
@@ -49,7 +49,7 @@ done
 COMPLETION_DIRS=(/etc/bash_completion.d/ \${HOME}/.bash_completion.d/)
 for COMPLETION_DIR in "\${COMPLETION_DIRS[@]}"; do
   if [[ -f "\${COMPLETION_DIR}" ]]; then
-    for f in \$(ls -1qA "\${COMPLETION_DIR}"); do    
+    for f in \$(ls -1qA "\${COMPLETION_DIR}"); do
       source "\${COMPLETION_DIR}/\${f}"
     done
   fi
@@ -97,9 +97,11 @@ create_ansible_venv() {
 }
 activate_ansible_venv() {
   cd $WORKDIR || exit 1
-  source ./.devcontainer/.venv-ansible/bin/activate
+  # shellcheck source=/dev/null
+  source .devcontainer/.venv-ansible/bin/activate
 }
 function install_ansible() {
+  python -m pip install --upgrade pip
   python -m pip install --upgrade ansible ansible-lint
 }
 function prepare_ansible_defaults() {
@@ -116,8 +118,64 @@ function test_initial_client_connection() {
   # ansible all -o -m ping
   ansible-playbook playbooks/ping_clients.yml
 }
-function install_pre_commit() {
-  .devcontainer/scripts/install-pre-commit.sh
+# function install_pre_commit() {
+#   .devcontainer/scripts/install-pre-commit.sh
+# }
+command_exists() {
+  # Function to check if a command exists
+  command -v "$1" >/dev/null 2>&1
+}
+install_pre_commit_linux() {
+  # Function to install pre-commit on Linux
+  echo "Installing pre-commit on Linux..."
+  $PIP_CMD install pre-commit
+}
+
+install_pre_commit_macos() {
+  # Function to install pre-commit on macOS
+  echo "Installing pre-commit on macOS..."
+  $PIP_CMD install pre-commit
+}
+check_pip_command() {
+  # Check if pip is installed
+  if command_exists pip; then
+    PIP_CMD="pip"
+  elif command_exists pip3; then
+    PIP_CMD="pip3"
+  else
+    echo "pip is not installed. Please install pip (https://pip.pypa.io/en/stable/installing/) and run the script again."
+    exit 1
+  fi
+}
+install_pre_commit() {
+  # Check if pre-commit is already installed
+  if command_exists pre-commit; then
+    echo "pre-commit is already installed."
+    return 0
+  else
+    case "$(uname)" in
+    Linux)
+      install_pre_commit_linux
+      ;;
+    Darwin)
+      # Check if Homebrew is installed
+      if command_exists brew; then
+        install_pre_commit_macos
+      else
+        echo "Homebrew is not installed. Please install Homebrew (https://brew.sh/) and run the script again."
+        exit 1
+      fi
+      ;;
+    *)
+      echo "Unsupported OS. This script only supports Linux and macOS."
+      exit 1
+      ;;
+    esac
+  fi
+}
+activate_pre_commit() {
+  pre-commit install
+  pre-commit autoupdate
 }
 function main() {
   install_basics
@@ -129,6 +187,8 @@ function main() {
   install_ansible
   prepare_ansible_defaults
   test_initial_client_connection
+  check_pip_command
   install_pre_commit
+  activate_pre_commit
 }
 main
